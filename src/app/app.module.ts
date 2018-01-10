c934da0506395328f32d5604a7bb58d48e55d312
Daniel Semac <dsemac@gmail.com>
2018-01-10 17:34:33 -0600

    Removed About page, contact page, removed tab icon captions, added charts caption, added chart.js, ng2-charts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,31 +3,31 @@ import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 
-import { AboutPage } from '../pages/about/about';
-import { ContactPage } from '../pages/contact/contact';
+import { ChartsPage } from '../pages/charts/charts';
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
+import { ChartsModule } from 'ng2-charts';
+
 @NgModule({
   declarations: [
     MyApp,
-    AboutPage,
-    ContactPage,
+    ChartsPage,
     HomePage,
     TabsPage
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    ChartsModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    AboutPage,
-    ContactPage,
+    ChartsPage,
     HomePage,
     TabsPage
   ],
