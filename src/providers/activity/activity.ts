53b7b23ef5a8a6a9ef1201da6276447d99ea58b0
Daniel Semac <dsemac@gmail.com>
2018-01-16 21:43:07 -0600

    activitiesref

--- src/providers/activity/activity.ts
+++ src/providers/activity/activity.ts
@@ -1,25 +1,33 @@
 import { Injectable } from '@angular/core';
-import { AngularFireDatabase } from 'angularfire2/database';
+import { AngularFireDatabase, AngularFireList } from 'angularfire2/database';
+import { Observable } from 'rxjs/Observable';
 
 @Injectable()
 export class ActivityProvider {
+  activitiesRef:  AngularFireList<any>;
+  logsRef: AngularFireList<any>;
 
   constructor(private afDatabase: AngularFireDatabase) {
     console.log('Hello ActivityProvider Provider');
   }
 
-  createDefaultActivities() {
+  setReferences(uid: string) {
+    this.activitiesRef = this.afDatabase.list(`activities/${uid}`);
+    this.logsRef = this.afDatabase.list(`logs/${uid}`);
+  }
 
+  createDefaultActivities(uid: string) {
+    this.activitiesRef.push('work');
+    this.activitiesRef.push('sleep');
+    this.activitiesRef.push('hobbies');
   }
 
   createActivity() {
 
   }
 
-  readActivities(id: string) {
-    let query: string = `activities/${id}`;
-
-    return this.afDatabase.list(query).valueChanges();
+  readActivities(id: string): Observable<any[]>{
+    return this.activitiesRef.valueChanges();
   }
 
   updateActivity() {
--- src/providers/auth/auth.ts
+++ src/providers/auth/auth.ts
@@ -1,5 +1,6 @@
 import { AngularFireAuth } from 'angularfire2/auth';
 import { Injectable } from '@angular/core';
+import { AngularFireDatabase } from 'angularfire2/database';
 import { ActivityProvider } from '../activity/activity';
 
 @Injectable()
@@ -7,7 +8,9 @@ export class AuthProvider {
 
   user: any;
 
-  constructor(private afAuth: AngularFireAuth, private activityProvider: ActivityProvider) {
+  constructor(private afAuth: AngularFireAuth, 
+              private afDatabase: AngularFireDatabase,
+              private activityProvider: ActivityProvider) {
     this.afAuth.authState.subscribe(user => {
       if(user) { 
         this.user = user; 
@@ -16,14 +19,19 @@ export class AuthProvider {
   }
 
   async login(email: string, password: string) {
-    this.user = this.afAuth.auth.signInWithEmailAndPassword(email, password);
-    var data = await this.activityProvider.readActivities(this.user.uid);
-
-    console.log(data);
+    this.user = await this.afAuth.auth.signInWithEmailAndPassword(email, password);
+    this.activityProvider.setReferences(this.user.uid);
   }
 
   async register(email: string, password: string) {
     this.user = await this.afAuth.auth.createUserWithEmailAndPassword(email, password);
+    if (this.user) {
+      this.afDatabase.database.ref('/users')
+      .child(this.user.uid)
+      .set({email: email });
+      this.activityProvider.setReferences(this.user.uid);
+      this.activityProvider.createDefaultActivities(this.user.uid);
+    }
   }
 
   async logout() {
