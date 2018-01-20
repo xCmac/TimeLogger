a8227d527272bafff1da1bf12746078a7c3e3f15
unknown <rachelvmadrigal@gmail.com>
2018-01-20 01:30:29 -0600

    Created color picker

---
+++ src/components/color-picker/color-picker.ts
@@ -0,0 +1,41 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'color-picker',
+  templateUrl: 'color-picker.html'
+})
+export class ColorPickerComponent {
+
+  @Input() current: string;
+  @Output() selected: EventEmitter<any> = new EventEmitter<any>();
+
+  colors: Array<string> = [
+    "red",
+    "pink",
+    "purple",
+    "deepPurple",
+    "indigo",
+    "blue",
+    "lightBlue",
+    "cyan",
+    "teal",
+    "green",
+    "lightGreen",
+    "lime",
+    "yellow",
+    "amber",
+    "orange",
+    "deepOrange",
+    "brown",
+    "grey", 
+    "blueGrey",
+    "black"
+  ]
+
+  constructor() {
+  }
+
+  selectColor(color: string) {
+    this.selected.emit(color);
+  }
+}
