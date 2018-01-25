b539a9fd789a7345614338ee21af9da3a907d0ad
unknown <rachelvmadrigal@gmail.com>
2018-01-25 00:16:24 -0600

    Updated users to Firestore

--- src/providers/auth/auth.ts
+++ src/providers/auth/auth.ts
@@ -1,20 +1,15 @@
 import { AngularFireAuth } from 'angularfire2/auth';
 import { Injectable } from '@angular/core';
-import { AngularFireDatabase } from 'angularfire2/database';
 import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument } from 'angularfire2/firestore';
 import { ActivityProvider } from '../activity/activity';
 import { LogProvider } from '../log/log';
 
 @Injectable()
 export class AuthProvider {
-
   user: any;
-
   usersCollection: AngularFirestoreCollection<any>;
 
-
   constructor(private afAuth: AngularFireAuth, 
-              private afDatabase: AngularFireDatabase,
               private afs: AngularFirestore,
               private activityProvider: ActivityProvider,
               private logProvider: LogProvider) {
@@ -36,14 +31,11 @@ export class AuthProvider {
   async register(email: string, password: string) {
     this.user = await this.afAuth.auth.createUserWithEmailAndPassword(email, password);
     if (this.user) {
-      this.afDatabase.database.ref('/users')
-      .child(this.user.uid)
-      .set({email: email });
       this.activityProvider.setReferences(this.user.uid);
       this.logProvider.setReferences(this.user.uid, new Date());
       this.activityProvider.createDefaultActivities(this.user.uid);
       
-      this.usersCollection.doc(this.user.uid).set({email: email});
+      this.usersCollection.add({email: email, userId: this.user.uid});
     }
   }
 
